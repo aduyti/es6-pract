61d5852d0dc8e7ba922f6d275adce706771aa995
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-19 20:05:33 +0600

    x

---
+++ yest.js
@@ -0,0 +1,14 @@
+const fname = "some";
+let age = 23;
+age = 25;
+const arr = [1, 2, 3, 4, 5, 7];
+//template string
+const st = `hello!
+${fname}
+${age}
+yo`;
+//spread operator
+const arr2 = [...arr, 56, 534, 2, 25];
+const yo = Math.max(...arr2);
+
+
