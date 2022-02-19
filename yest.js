a54865aba5a1b94b7cacbb3cfb8a8ffcc6c0f829
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-19 20:13:20 +0600

    h

--- yest.js
+++ yest.js
@@ -10,5 +10,15 @@ yo`;
 //spread operator
 const arr2 = [...arr, 56, 534, 2, 25];
 const yo = Math.max(...arr2);
+//arrow function
+const func = () => 1;
+const func3 = x => x - 1;
+const func2 = (x = 0) => x - 1; // default parameter value
+const func1 = (x, y, z) => x + y + z;
+const func11 = (x, y, z) => {
+    x + y + z;
+    return 0;
+}
 
+console.log(func2(6));
 
