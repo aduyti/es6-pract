939d3b5c37dae1b7ef89e18cad962d581db63ed7
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-20 21:25:39 +0600

    obj

---
+++ object.js
@@ -0,0 +1,12 @@
+const myLap = {
+    processor: 'i7',
+    ram: '8GB',
+    storage: '2TB',
+    graphics: '4GB',
+    display: '17inch',
+    price: 43000
+};
+// destructuring 
+const { processor, ram, graphics } = myLap;
+const [a, b, c, d, e] = [1, 2, .2, 25, 55];
+const [x, y] = [1, 2, .2, 25, 55];
