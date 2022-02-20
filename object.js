d4b119165d5976ab064dfa18145f60242dcaf76a
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-20 23:49:49 +0600

    map

--- object.js
+++ object.js
@@ -14,7 +14,7 @@ const myLap = {
 const accessory = {
     keyboard: 'A4Tech',
     mouse: 'A4Tech',
-}
+};
 // destructuring 
 const { processor, ram, graphics } = myLap;
 const [a, b, c, d, e] = [1, 2, .2, 25, 55];
@@ -24,4 +24,14 @@ const xv = myLap?.processor?.name;
 const xy = myLap?.processor?.cache;
 // spread operator on object
 const mySetup = { ...myLap, ...accessory };
-console.log(mySetup);
+// console.log(mySetup);
+const players = [
+    { name: { firstName: 'John', lastName: 'Doe' }, type: 'none', age: 23 },
+    { name: { firstName: 'Sakib', lastName: 'Hasan' }, type: 'cricket', age: 35 },
+    { name: { firstName: 'Mas', lastName: 'Taja' }, type: 'cricket', age: 38 },
+    { name: { firstName: 'Mara', lastName: 'Dona' }, type: 'football', age: 70 },
+    { name: { firstName: 'Jor', lastName: 'Dan' }, type: 'basketball', age: 58 },
+];
+
+const age = players.map(player => player.age > 30);
+console.log(age);
